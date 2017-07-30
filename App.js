5910c6711bb457b66acf3f832a55bc85e912d3de
ragtimekeys <ragtimekeys@gmail.com>
2017-07-30 16:23:08 -0500

    Change code structure

--- App.js
+++ App.js
@@ -4,8 +4,7 @@
 
 
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-import { AppRegistry, ScrollView, Image } from 'react-native';
+import { StyleSheet, Text, View, AppRegistry, ScrollView, Image} from 'react-native';
 import { Constants } from 'expo';
 
 export default class App extends React.Component {
